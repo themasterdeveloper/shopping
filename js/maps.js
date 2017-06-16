fcc9cd303c7d70f40c4fb281d1fa4549a0ff31a6
themasterdeveloper <omar.melendrez@gmail.com>
2017-06-16 15:06:34 -0300

    Implement object methods for maps

--- js/maps.js
+++ js/maps.js
@@ -1,135 +1,157 @@
-const FAST_FOOD = '/img/rsz_fastfood-pin-2.png';
-const DELIVERY_MAN = '/img/fastfood-worker-64-255355.png';
+var maps = {
+	ICON_FAST_FOOD: '/img/rsz_fastfood-pin-2.png',
+	ICON_DELIVERY_MAN: '/img/fastfood-worker-64-255355.png',
 
-save_area_location = function(area_id, lat, lng) {
-    var data = {};
-    data.action = "save_area_location";
-    data.area_id = area_id;
-    data.lat = lat;
-    data.lng = lng;
-    log("save_area_location", data);
-    $.ajax({
-        data: data,
-        dataType: 'json',
-        success: function(data) {
-            log("save_area_location", data);
-            if (data[0].error != 0) {
-                showErr(data[0].message);
-            } else {
-                showMsg(data[0].message);
-            }
-        }
-    });
-}
+	save_area_location: function(area_id, lat, lng) {
+		var data = {
+			action: "save_area_location",
+			area_id: area_id,
+			lat: lat,
+			lng: lng
+		}
 
-save_shop_location = function(area_id, shop_id, lat, lng) {
-    var data = {};
-    data.action = "save_shop_location";
-    data.area_id = area_id;
-    data.shop_id = shop_id;
-    data.lat = lat;
-    data.lng = lng;
-    log("save_shop_location", data);
-    $.ajax({
-        data: data,
-        dataType: 'json',
-        success: function(data) {
-            log("save_shop_location", data);
-            if (data[0].error != 0) {
-                showErr(data[0].message);
-            } else {
-                showMsg(data[0].message);
-                update_markers();
-            }
-        }
-    });
-}
+		log("save_area_location", data);
 
-load_shops_locations = function(area_id) {
-    var data = {};
-    data.action = "shops_locations_list";
-    data.area_id = 0;
-    log("load_shops_locations", data);
-    $.ajax({
-        data: data,
-        success: function(data) {
-            log("load_shops_locations", data);
-            setMarkers(data, 'shop', FAST_FOOD);
-        }
-    });
-}
+		$.ajax({
+			data: data,
+			dataType: 'json',
+			success: function(data) {
+				log("save_area_location", data);
+				if (data[0].error != 0) {
+					showErr(data[0].message);
+				} else {
+					showMsg(data[0].message);
+				}
+			}
+		});
+	},
 
-load_deliverers_locations = function(area_id) {
-    var data = {};
-    data.action = "deliverers_list";
-    log("deliverers_list", data);
-    $.ajax({
-        data: data,
-        success: function(data) {
-            log("deliverers_list", data);
-            setMarkers(data, 'name', DELIVERY_MAN);
-        }
-    });
-}
+	save_shop_location: function(area_id, shop_id, lat, lng) {
+		var data = {
+			action: "save_shop_location",
+			area_id: area_id,
+			shop_id: shop_id,
+			lat: lat,
+			lng: lng
+		}
 
-setMarkers = function(list, field, image) {
-    for (var i = 0; i < list.length; i++) {
-        var item = list[i];
-        var siteLatLng = new google.maps.LatLng(item["lat"], item["lng"]);
-        createMarker(siteLatLng, field, image, item);
-    }
-}
+		log("save_shop_location", data);
 
-createMarker = function(latlng, field, image, data) {
-    var marker = new google.maps.Marker({
-        position: latlng,
-        map: map,
-        title: data[field],
-        icon: image
-    });
-    marker.set('id', data['id']);
-    marker.addListener('click', function() {
-        $('#item_id').val(data['id']);
-        showMarkerData(data);
-    });
-    markers.push(marker);
-}
+		$.ajax({
+			data: data,
+			dataType: 'json',
+			success: function(data) {
+				log("save_shop_location", data);
+				if (data[0].error != 0) {
+					showErr(data[0].message);
+				} else {
+					showMsg(data[0].message);
+					maps.update_markers();
+				}
+			}
+		});
+	},
 
-showMarkerData = function(data) {
-    var html = '';
-    $this = data[0];
-    var skip_columns = "-id-lat-lng-photo-available-";
-    for (var key in data) {
-        if (skip_columns.indexOf("-" + key + "-") == -1) {
-            html += '<span class="_data-value">';
-            html += data[key];
-            html += '</span></br>';
-        }
-    }
-    $('.marker-data').html(html);
-}
+	load_shops_locations: function(area_id) {
+		var data = {
+			action: "shops_locations_list",
+			area_id: 0
+		}
 
-remove_markers = function() {
-    for (i = 0; i < markers.length; i++) {
-        markers[i].setMap(null);
-    }
-}
+		log("load_shops_locations", data);
 
-close_infowindows = function() {
-    for (i = 0; i < infowindows.length; i++) {
-        infowindows[i].close();
-    }
-}
+		$.ajax({
+			data: data,
+			success: function(data) {
+				log("load_shops_locations", data);
+				maps.setMarkers(data, 'shop', maps.FAST_FOOD);
+			}
+		});
+	},
 
-relocateMap = function() {
+	load_deliverers_locations: function(area_id) {
+		var data = {
+			action: "deliverers_list"
+		}
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            lat = position.coords.latitude;
-            lng = position.coords.longitude;
-            initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            //map.setCenter(initialLocation);
-            me_marker.setPosition(initialLocation);
-        });
-    }
+		log("deliverers_list", data);
+
+		$.ajax({
+			data: data,
+			success: function(data) {
+				log("deliverers_list", data);
+				maps.setMarkers(data, 'name', maps.DELIVERY_MAN);
+			}
+		});
+	},
+
+	setMarkers: function(list, field, image) {
+		for (var i = 0; i < list.length; i++) {
+			var item = list[i];
+			var siteLatLng = new google.maps.LatLng(item["lat"], item["lng"]);
+			maps.createMarker(siteLatLng, field, image, item);
+		}
+	},
+
+	createMarker: function(latlng, field, image, data) {
+		var marker = new google.maps.Marker({
+			position: latlng,
+			map: map,
+			title: data[field],
+			icon: image
+		});
+		marker.set('id', data['id']);
+		marker.addListener('click', function() {
+			$('#item_id').val(data['id']);
+			maps.showMarkerData(data);
+		});
+		markers.push(marker);
+	},
+
+	showMarkerData: function(data) {
+		var html = '';
+		$this = data[0];
+		var skip_columns = "-id-lat-lng-photo-available-";
+		for (var key in data) {
+			if (skip_columns.indexOf("-" + key + "-") == -1) {
+				html += '<span class="_data-value">';
+				html += data[key];
+				html += '</span></br>';
+			}
+		}
+		$('.marker-data').html(html);
+	},
+
+	remove_markers: function() {
+		for (i = 0; i < markers.length; i++) {
+			markers[i].setMap(null);
+		}
+	},
+
+	close_infowindows: function() {
+		for (i = 0; i < infowindows.length; i++) {
+			infowindows[i].close();
+		}
+	},
+
+	relocateMap: function() {
+
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function(position) {
+				lat = position.coords.latitude;
+				lng = position.coords.longitude;
+				initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+				//map.setCenter(initialLocation);
+				me_marker.setPosition(initialLocation);
+			});
+		}
+	},
+
+	update_markers: function() {
+
+		maps.remove_markers();
+		maps.load_shops_locations(0);
+	}
+
+
 }
